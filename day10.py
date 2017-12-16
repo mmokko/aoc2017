fdafe6e11726a22782a65b4e997e557cf4f4b7b2
Matti Mokko <matti.mokko@gmail.com>
2017-12-16 19:30:49 +0200

    Day10 second puzzle solution

--- day10.py
+++ day10.py
@@ -1,3 +1,12 @@
+from functools import reduce
+
+
+DEFAULT_ASCII_INPUTS = [17, 31, 73, 47, 23]
+ROUNDS = 64
+SUB_BLOCKS = 16
+BLOCK_SIZE = 16
+
+
 class KnotHasher(object):
     def __init__(self, hash_size, inputs):
         self._hash = [idx for idx in range(0, hash_size)]
@@ -28,9 +37,9 @@ class KnotHasher(object):
     def _get_idx_end(self, value):
         if value + self._idx < len(self._hash):
             return self._idx + value
-        return self._idx + value - len(self._hash)
+        return (self._idx + value) % len(self._hash)
 
-    def hash(self):
+    def calc(self):
         for value in self._inputs:
             if value: # ignore zero values as they don't have effect
                 self._hash_with_input(value)
@@ -38,11 +47,50 @@ class KnotHasher(object):
             self._skip += 1
         return self._hash[0] * self._hash[1]
 
+    def _get_ascii_input(self):
+        ascii_list = [ord(x) for x in self._inputs]
+        return ascii_list + DEFAULT_ASCII_INPUTS
 
-def main():
+    def _get_sub_block(self, idx):
+        start = idx * BLOCK_SIZE
+        end = start + BLOCK_SIZE
+        return self._hash[start : end]
+
+    def _create_dense_hash(self):
+        dense_hash = list()
+        for idx in range(0, SUB_BLOCKS):
+            block = self._get_sub_block(idx)
+            dense_hash.append(reduce(lambda x, y: x ^ y, block))
+        return dense_hash
+
+    def _ascii_hash(self, dense_hash):
+        return hash
+
+    def hash(self):
+        self._inputs = self._get_ascii_input()
+        round = 0
+        while round < ROUNDS:
+            self.calc()
+            round += 1
+        dense_hash = self._create_dense_hash()
+        return ''.join('{:02x}'.format(x) for x in dense_hash)
+
+
+def puzzle1():
     puzzle_input = [106, 16, 254, 226, 55, 2, 1, 166, 177, 247, 93, 0, 255, 228, 60, 36]
     knot_hasher = KnotHasher(256, puzzle_input)
+    print(knot_hasher.calc())
+
+
+def puzzle2():
+    puzzle_input = '106,16,254,226,55,2,1,166,177,247,93,0,255,228,60,36'
+    knot_hasher = KnotHasher(256, puzzle_input)
     print(knot_hasher.hash())
+
+
+def main():
+    puzzle1()
+    puzzle2()
 
 
 if __name__== '__main__':
--- test_knot_hasher.py
+++ test_knot_hasher.py
@@ -3,6 +3,22 @@ from day10 import KnotHasher
 
 
 class TestKnotHasher(TestCase):
-    def test_hash(self):
+    def test_calc(self):
         sut = KnotHasher(5, [3, 4, 1, 5])
-        self.assertEqual(12, sut.hash())
+        self.assertEqual(12, sut.calc())
+
+    def test_hash1(self):
+        sut = KnotHasher(256, '')
+        self.assertEqual('a2582a3a0e66e6e86e3812dcb672a272', sut.hash())
+
+    def test_hash2(self):
+        sut = KnotHasher(256, 'AoC 2017')
+        self.assertEqual('33efeb34ea91902bb2f59c9920caa6cd', sut.hash())
+
+    def test_hash3(self):
+        sut = KnotHasher(256, '1,2,3')
+        self.assertEqual('3efbe78a8d82f29979031a4aa0b16a9d', sut.hash())
+
+    def test_hash4(self):
+        sut = KnotHasher(256, '1,2,4')
+        self.assertEqual('63960835bcdc130f0b66d7ff4f6a5a8e', sut.hash())
